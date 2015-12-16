bfc80260eafc4e3896097d104604e4ae88729dea
Ritesh H Shukla <sritesh@vmware.com>
2015-12-16 11:38:48 -0800

    Get docker builds going. Customize zsh theme.

--- firstrun.sh
+++ firstrun.sh
@@ -6,4 +6,9 @@ docker run -it \
 	   -v $HOME/src:/root/dev \
 	   -v $HOME/.ssh:/root/.ssh \
 	   -v /etc/localtime:/etc/localtime:ro \
-	   kerneltime/dev 
+	   -v /var/run/docker.sock:/var/run/docker.sock \
+           -v $(which docker):/bin/docker \
+	   -v $(ldconfig -p | grep 'libdevmapper.so.1.02' | awk '{print $4}'):/usr/lib/libdevmapper.so.1.02 \
+           -e LD_LIBRARY_PATH=/usr/lib \
+	   kerneltime/dev  /bin/bash -c "ldconfig; sed -i  's/ZSH_THEME=.*/ZSH_THEME=candy/g' ~/.zshrc; ZSH_THEME=candy zsh"
+
