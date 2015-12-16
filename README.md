d2d71e116e253d8d5720ac40d4a036e16604374a
Ritesh H Shukla <sritesh@vmware.com>
2015-12-15 16:24:28 -0800

    Updates for reliable runs. Add rustfmt and rlwrap. Customize git

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # DevSetup
 
 Using Docker to reproduce my dev environment. 
+
+**Customize the Dockerfile.dev file before use**
+
 This is WIP.
--- firstrun.sh
+++ firstrun.sh
@@ -1,7 +1,9 @@
 # Using run vs create and then start..
+docker rm kerneltime.dev
 docker run -it \
 	   --name kerneltime.dev \
            -w /root/dev \
 	   -v $HOME/src:/root/dev \
 	   -v $HOME/.ssh:/root/.ssh \
+	   -v /etc/localtime:/etc/localtime:ro \
 	   kerneltime/dev 
