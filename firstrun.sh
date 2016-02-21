5ba4ee8105040141e3dab9fafc6506b9d2de67c4
Ritesh H Shukla <sritesh@vmware.com>
2016-02-20 23:24:30 -0800

    Add ability to make code changes inside as non root. Add support for DinD using docker installed within the image.

--- firstrun.sh
+++ firstrun.sh
@@ -2,16 +2,13 @@
 docker rm kerneltime.dev
 docker run -it \
 	   --name kerneltime.dev \
-           -w /root/dev \
-	   -v $HOME/src:/root/dev \
-	   -v $HOME/.ssh:/root/.ssh \
+           -w /home/ritesh/dev \
+	   -v $HOME/dev:/home/ritesh/dev \
+	   -v $HOME/.ssh:/home/ritesh/.ssh \
+           -v $HOME/go:/home/ritesh/go \
 	   -v /etc/localtime:/etc/localtime:ro \
 	   -v /var/run/docker.sock:/var/run/docker.sock \
-           -v $(which docker):/bin/docker \
-	   -v $(ldconfig -p | grep 'libdevmapper.so.1.02' | awk '{print $4}'):/usr/lib/libdevmapper.so.1.02 \
-           -e LD_LIBRARY_PATH=/usr/lib \
-	   kerneltime/dev  /bin/bash -c "ldconfig ; \
-					 git config --global user.email \"sritesh@vmware.com\"; \
+	   kerneltime/dev  /bin/bash -c "git config --global user.email \"sritesh@vmware.com\"; \
 				         git config --global user.name \"Ritesh H Shukla\"; \
 					 sed -i 's/ZSH_THEME=.*/ZSH_THEME=candy/g' ~/.zshrc;
 					 zsh"
